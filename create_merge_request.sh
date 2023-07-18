af102274f9eca5e47db77a54aa9732609b476d6c
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2023-07-18 11:12:08 -0400

    Use same token variable

--- create_merge_request.sh
+++ create_merge_request.sh
@@ -5,5 +5,5 @@ mr_title="$3"
 # Reference - https://docs.gitlab.com/ee/api/merge_requests.html#create-mr
 curl --request POST \
      --data "source_branch=${source_branch}&target_branch=${target_branch}&title=${mr_title}" \
-     --header "PRIVATE-TOKEN: ${PROJECT_TOKEN}" \
+     --header "PRIVATE-TOKEN: ${PERSONAL_TOKEN}" \
      "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/merge_requests"
